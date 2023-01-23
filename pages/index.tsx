cd4a68eb1a8b1a6634d94c15f191a31f3802f07b
MindaugasPaskevicius <mindaugas.paske@gmail.com>
2023-01-23 16:24:24 +0200

    create PostWidget component

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,13 +1,10 @@
 import type { NextPage } from "next";
 import Head from "next/head";
 import {Categories, PostCard, PostWidget} from '../components';
+import { getPosts } from '../services'
 
-const posts = [
-  { title: "React Testing", excerpt: "Learn React Testing" },
-  { title: "React with Tailwind", excerpt: "Learn React with Tailwind" },
-];
 
-const Home: NextPage = () => {
+const Home: NextPage = ({posts}) => {
   return (
     <div className="container mx-auto px-10 mb-8">
       <Head>
@@ -16,10 +13,10 @@ const Home: NextPage = () => {
       </Head>
       <div className="grid grid-cols-1 lg:grid-cols-12 gap-12">
         <div className="lg:col-span-8 col-span-1">
-           {posts.map((post) => <PostCard post={post} key={post.title} />)}
+           {posts.map((post, index) => <PostCard key={`post-card-${index}`} post={post.node} key={post.title} />)}
         </div>
         <div className="lg:col-span-4 col-span-1">
-           <div className="lg:sticky relatve top-8">
+           <div className="lg:sticky relative top-8">
               <PostWidget />
               <Categories />
            </div>
@@ -29,5 +26,13 @@ const Home: NextPage = () => {
     </div>
   );
 };
+
+export async function getStaticProps() {
+  const posts = (await getPosts()) || [];
+
+  return {
+    props: { posts },
+  };
+}
 
 export default Home;
