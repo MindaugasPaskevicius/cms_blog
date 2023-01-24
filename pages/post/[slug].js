6376a36ff7727ccf15d6d9d3169a60880d041672
MindaugasPaskevicius <mindaugas.paske@gmail.com>
2023-01-24 11:23:47 +0200

    finished PostDetails component

--- pages/post/[slug].js
+++ pages/post/[slug].js
@@ -2,7 +2,7 @@ import React from 'react'
 import { PostDetail, Categories, PostWidget, Author, Comments, CommentsForm, Loader } from '../../components';
 import { getPosts, getPostDetails } from '../../services';
 
-const PostDetails = () => {
+const PostDetails = ({ post }) => {
   return (
     <>
     <div className="container mx-auto px-10 mb-8">
@@ -10,7 +10,7 @@ const PostDetails = () => {
         <div className="col-span-1 lg:col-span-8">
           <PostDetail post={post} />
           <Author author={post.author} />
-          <AdjacentPosts slug={post.slug} createdAt={post.createdAt} />
+          {/* <AdjacentPosts slug={post.slug} createdAt={post.createdAt} /> */}
           <CommentsForm slug={post.slug} />
           <Comments slug={post.slug} />
         </div>
@@ -29,3 +29,19 @@ const PostDetails = () => {
 
 
 export default PostDetails;
+
+export async function getStaticProps({ params} ) {
+  const data = await getPostDetails(params.slug);
+
+  return {
+    props: { post: data },
+  };
+}
+
+export async function getStaticPaths() {
+  const posts = await getPosts();
+  return {
+    paths: posts.map(({ node: { slug } }) => ({ params: { slug } })),
+    fallback: true,
+  };
+}
